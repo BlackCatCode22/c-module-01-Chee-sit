cda663bf6785ecc29c1dd3bda7cfa3631c4b29c8
Chee-sit <dmyanvang1@gmail.com>
2025-08-13 11:41:26 -0700

    Add files via upload

---
+++ helloWorldReview.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    cout << "HelloWorldReview";
+
+    int a = 4;
+    int b = 5;
+
+    cout << "a = " << a;
+    cout << "b = " << b;
+
+    int sum = a + b;
+
+    cout << "sum = " << sum;
+
+
+    return 0;
+
+
+}
