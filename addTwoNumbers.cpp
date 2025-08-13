2bdc3a8eaa1f94e075fa76233bcae4274b5c64ed
Chee-sit <dmyanvang1@gmail.com>
2025-08-13 12:24:44 -0700

    Add files via upload

---
+++ addTwoNumbers.cpp
@@ -0,0 +1,23 @@
+// created by Chijenda Vang on 8/13/2025
+// addTwoNumbers.cpp
+#include <iostream>
+using namespace std;
+
+// create a very important function named main
+// this is where your program starts
+int main() {
+
+    // create variables needed for this program.
+    int sumOFNums = 0;
+    int num1 = 0;
+    int num2 = 0;
+    cout << "Enter First Number: ";
+    num1 = cin.get();
+    cout << "Enter Second Number: ";
+    num2 = cin.get();
+    sumOFNums = num1 + num2;
+    cout << "The sum of the two numbers: " << sumOFNums << endl;
+
+
+    return 0;
+}
